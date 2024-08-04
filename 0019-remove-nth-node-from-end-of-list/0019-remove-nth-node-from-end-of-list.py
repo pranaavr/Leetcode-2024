4088d9e684624c66a38a779a8a29bfa4d527d24a
pranaavr <68622267+pranaavr@users.noreply.github.com>
2024-08-04 15:36:19 -0400

    Time: 12 ms (82.25%), Space: 11.5 MB (69.49%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,22 +1,30 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        curr = head
-        lst = []
-        while curr:
-            lst.append(curr.val)
-            curr = curr.next
-        del lst[-n]
-        head, curr = None, None
-        for l in lst:
-            if not head:
-                head = ListNode(l)
-                curr = head
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        sentinel = ListNode()
+        sentinel.next = head
+        slow = sentinel
+        fast = sentinel
+        
+        for _ in range(n + 1):
+            if fast:
+                fast = fast.next
             else:
-                curr.next = ListNode(l)
-                curr = curr.next
-        return head
+                return head
+        
+        while fast:
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next
+
+        return sentinel.next
