cd51abadbc05ec390271c3abb1da2dd9b49ae0fe
pranaavr <68622267+pranaavr@users.noreply.github.com>
2024-08-05 22:08:39 -0400

    Time: 42 ms (71%), Space: 11.6 MB (64.45%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,25 +10,38 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        s1 = []
-        curr = l1
-        while curr:
-            s1.append(str(curr.val))
-            curr = curr.next
-        s2 = []
-        curr = l2
-        while curr:
-            s2.append(str(curr.val))
-            curr = curr.next
-        num1 = int(''.join(s1[::-1]))
-        num2 = int(''.join(s2[::-1]))
-        sm = str(num1+num2)
-        sm = sm[::-1]
+        # def reverse(head):
+        #     curr = head
+        #     prev = None
+        #     while curr:
+        #         nxt = curr.next
+        #         curr.next = prev
+        #         prev = curr
+        #         curr = nxt
+        #     return prev
 
-        dummy = ListNode(0)
+        # l1 = reverse(l1)
+        # l2 = reverse(l2)
+
+        dummy = ListNode()
         curr = dummy
-        for i in sm:
-            curr.next = ListNode(int(i))
-            curr = curr.next
+        carry = 0
+        
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            sm = val1 + val2 + carry
+            carry = sm // 10
+            sm = sm % 10
 
+            curr.next = ListNode(sm)
+            curr = curr.next
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+        
+        if carry:
+            curr.next = ListNode(carry)
+        
         return dummy.next
+        
+            
