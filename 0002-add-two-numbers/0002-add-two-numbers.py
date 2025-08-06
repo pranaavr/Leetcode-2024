781e77ba64bdb55b9f1039a7d4c5f4478385382a
pranaavr <68622267+pranaavr@users.noreply.github.com>
2025-08-06 09:22:13 -0400

    Time: 1 ms (82.29%), Space: 17.9 MB (56.6%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,47 +1,27 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        # def reverse(head):
-        #     curr = head
-        #     prev = None
-        #     while curr:
-        #         nxt = curr.next
-        #         curr.next = prev
-        #         prev = curr
-        #         curr = nxt
-        #     return prev
-
-        # l1 = reverse(l1)
-        # l2 = reverse(l2)
-
-        dummy = ListNode()
-        curr = dummy
-        carry = 0
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = newl = ListNode()
         
-        while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
-            sm = val1 + val2 + carry
-            carry = sm // 10
-            sm = sm % 10
-
-            curr.next = ListNode(sm)
-            curr = curr.next
-            if l1: l1 = l1.next
-            if l2: l2 = l2.next
-        
-        if carry:
-            curr.next = ListNode(carry)
-        
-        return dummy.next
-        
+        while l1 or l2:
+            if l1:
+                newl.val += l1.val
+                l1 = l1.next
+            if l2:
+                newl.val += l2.val            
+                l2 = l2.next
             
+            if newl.val >= 10:
+                newl.val -= 10
+                newl.next = ListNode(1)
+            elif l1 or l2:
+                newl.next = ListNode()
+            newl = newl.next
+        
+        return dummy
+
+        
