7217cf45e9bd33ba353ed6287db317c42c6c0318
pranaavr <68622267+pranaavr@users.noreply.github.com>
2026-01-09 21:47:02 -0500

    Time: 55 ms (69.01%), Space: 27.6 MB (5.37%) - LeetHub

---
+++ 3101-count-alternating-subarrays/3101-count-alternating-subarrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countAlternatingSubarrays(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1] * n
+        
+        for i in range(1, n):
+            if nums[i-1] != nums[i]:
+                dp[i] = dp[i - 1] + 1
+        
+        return sum(dp)
+
+
+        
