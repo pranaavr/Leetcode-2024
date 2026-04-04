081d61edf36f261b90a9f91ab8c7e715f5a9bcb8
pranaavr <68622267+pranaavr@users.noreply.github.com>
2026-04-04 11:57:24 -0400

    Time: 179 ms (38%), Space: 20.6 MB (60.92%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,26 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        piles.sort()
+
+        l = 1
+        r = piles[-1]
+
+        best = l
+        while l <= r:
+            k = (l+r)//2    # bananas per hour
+            
+            # calculate total hours
+            tot = 0
+            for p in piles:
+                tot += math.ceil(p/k)
+            
+            if tot <= h: # if total hours less than h, eating speed needs to decrease
+                best = k
+                r = k - 1
+            else: # if total hours more than h, eating speed needs to increase
+                l = k + 1
+        
+        return best
+            
