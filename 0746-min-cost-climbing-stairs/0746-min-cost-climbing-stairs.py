7577fa70ff5180beaf6e1a1fdbb28bc66479dd1c
pranaavr <68622267+pranaavr@users.noreply.github.com>
2026-05-25 18:16:51 -0400

    Time: 3 ms (63.17%), Space: 19.3 MB (48.63%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,23 +1,9 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         
-        n = len(cost)
-        cost.append(0)
-
-        mem = {}
-        # n-1 or n-2
-        def rec(n):
-            if n in mem:
-                return mem[n]
-            if n == 0:
-                return cost[0]
-            if n == 1:
-                return cost[1]
-            if rec(n-1) < rec(n-2):
-                mem[n] = cost[n] + rec(n-1)
-                return mem[n]
-            else:
-                mem[n] = cost[n] + rec(n-2)
-                return mem[n]            
+        for i in range(len(cost)):
+            if i <= 1:
+                continue
+            cost[i] += min(cost[i-1], cost[i-2])
         
-        return rec(n)
+        return min(cost[-1], cost[-2])
