ad570fc9a509da698d26e41deb2c37cb425049c4
pranaavr <68622267+pranaavr@users.noreply.github.com>
2026-05-25 16:39:49 -0400

    Time: 3 ms (75.18%), Space: 20.3 MB (89.96%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # map of prereqs for each course
+        graph = defaultdict(list)
+        # num prereqs for each course
+        indegree = [0]*numCourses
+
+        for course, pre in prerequisites:
+            graph[pre].append(course)
+            indegree[course] += 1
+        
+        # only queue courses with no prereqs
+        queue = deque([i for i in range(numCourses) if indegree[i]==0])
+        order = []
+
+        while queue:
+            course = queue.popleft()
+            order.append(course)
+
+            for child in graph[course]:
+                indegree[child] -= 1
+                if indegree[child] == 0:    # check if all prereqs for the course are met
+                    queue.append(child)
+        
+        return order if sum(indegree) == 0 else []
+
