aa8db8e3fdfecfcdaee117889482a4f9d1adbb36
pranaavr <68622267+pranaavr@users.noreply.github.com>
2026-05-31 20:11:16 -0400

    Time: 3 ms (75%), Space: 20.3 MB (73.52%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,26 +1,25 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # map of prereqs for each course
-        graph = defaultdict(list)
-        # num prereqs for each course
-        indegree = [0]*numCourses
-
-        for course, pre in prerequisites:
-            graph[pre].append(course)
-            indegree[course] += 1
         
-        # only queue courses with no prereqs
-        queue = deque([i for i in range(numCourses) if indegree[i]==0])
-        order = []
-
-        while queue:
-            course = queue.popleft()
-            order.append(course)
-
-            for child in graph[course]:
-                indegree[child] -= 1
-                if indegree[child] == 0:    # check if all prereqs for the course are met
-                    queue.append(child)
+        pre = defaultdict(list) # track what courses the key is a prereq for
+        indegrees = [0]*numCourses
+        for u, v in prerequisites:
+            pre[v].append(u)
+            indegrees[u] += 1   # how many prereqs the coursse has
         
-        return order if len(order) == numCourses else []
+        q = deque() # tracking for course with all prereqs met
 
+        for i in range(len(indegrees)):
+            if indegrees[i] == 0:
+                q.append(i)
+        
+        res = []
+        while q:
+            cur = q.popleft()
+            res.append(cur)
+            for course in pre[cur]:
+                indegrees[course] -= 1
+                if indegrees[course] == 0:
+                    q.append(course)
+        
+        return res if sum(indegrees) == 0 else []
